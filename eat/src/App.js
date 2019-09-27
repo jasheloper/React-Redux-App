8130b49bc48cdd78958f66ab47fc757fa031c638
Jashele T <jashele@Jasheles-Air.domain>
2019-09-26 20:29:41 -0700

    mvp complete

--- eat/src/App.js
+++ eat/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Meal from './Components/Meal';
+import Footer from './Components/footer'
 
 function App() {
   return (
     <div className="App">
           <Meal />
+          <Footer />
     </div>
   );
 }
