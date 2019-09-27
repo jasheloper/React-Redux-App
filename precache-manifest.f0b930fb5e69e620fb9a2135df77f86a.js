8dabd49ad3e6c9afd50aa9fa852fbe8f74d91785
Jashele T <jashele@Jasheles-Air.domain>
2019-09-26 20:34:03 -0700

    Updates

---
+++ precache-manifest.f0b930fb5e69e620fb9a2135df77f86a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c4b85d04775daf522adc6418344c44",
+    "url": "/React-Redux-App/index.html"
+  },
+  {
+    "revision": "a284f222663405050699",
+    "url": "/React-Redux-App/static/css/main.23b0f03c.chunk.css"
+  },
+  {
+    "revision": "e6e53921d663a7bda2f3",
+    "url": "/React-Redux-App/static/js/2.23a3a133.chunk.js"
+  },
+  {
+    "revision": "a284f222663405050699",
+    "url": "/React-Redux-App/static/js/main.e08de2e6.chunk.js"
+  },
+  {
+    "revision": "3862bb59fce24239ee05",
+    "url": "/React-Redux-App/static/js/runtime-main.dfd1da47.js"
+  }
+]);
